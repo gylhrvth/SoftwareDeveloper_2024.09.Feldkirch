1cb0d3e3cbc4b75e5376be2002bd11ecf6b11578
Aylin <topcuazlin98@gmail.com>
2024-09-09 10:48:22 +0200

    printA

--- src/Aylin/week01/printA.java
+++ src/Aylin/week01/printA.java
@@ -3,7 +3,7 @@ package Aylin.week01;
 public class printA {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("");
+        System.out.println();
         System.out.println("     /--\\");
         System.out.println("    / /\\ \\");
         System.out.println("   / /  \\ \\");
