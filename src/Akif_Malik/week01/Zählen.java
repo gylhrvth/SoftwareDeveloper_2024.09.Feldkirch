c0a6f9d5e7f786e5ffde509066db1103f8761f24
Akif <akifuenalan@gmail.com>
2024-09-09 12:15:10 +0200

    Zählen

---
+++ src/Akif_Malik/week01/Zählen.java
@@ -0,0 +1,16 @@
+package Akif_Malik.week01;
+
+public class Zählen {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; ++i) {
+            System.out.println(+i);
+        }
+
+        int j = 0;
+        while (j < 11) {
+            System.out.println(j);
+            ++j;
+        }
+
+    }
+}
