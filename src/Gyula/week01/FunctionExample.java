ede578b32f8ea4a4ab240e6abde68df56dd862d2
Gyula Horvath <gylhrvth@gmail.com>
2024-09-09 14:35:20 +0200

    Function Examples

---
+++ src/Gyula/week01/FunctionExample.java
@@ -0,0 +1,36 @@
+package Gyula.week01;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+        sayHello();
+        System.out.println("-----");
+        repeatHello(5);
+        repeatHello(2);
+        System.out.println("-----");
+        int summe = addNumber(5, 2);
+        System.out.println("Summe = " + summe);
+        System.out.println("------");
+        sayHelloTo("Gyula", 2);
+    }
+
+
+    public static void sayHello(){
+        System.out.println("Hello!");
+    }
+
+    public static void repeatHello(int count){
+        for (int i = 0; i < count; i++) {
+            System.out.println("Hello!");
+        }
+    }
+
+    public static int addNumber(int a, int b){
+        return a + b;
+    }
+
+    public static void sayHelloTo(String name, int count){
+        for (int i = 0; i < count; ++i){
+            System.out.println("Hello " + name + "!");
+        }
+    }
+}
