8319406f7c483efe0d7dc34f0f33b2803003327c
Thiemo <thiemo.hofacker@gmail.com>
2024-09-09 20:57:26 +0200

    Number countig

--- src/Thiemo/week01/CountTheNumbers.java
+++ src/Thiemo/week01/CountTheNumbers.java
@@ -15,18 +15,24 @@ public class CountTheNumbers {
         ++countLoops;
         System.out.println("Count loops is " + countLoops);
 
+        System.out.println("");
+        System.out.println("Übung1");
+        System.out.println("");
         for (int i = 1; i < 10; ++i) {
 
             System.out.println("Yeah" + i);
         }
-
-
+        System.out.println("");
+        System.out.println("Übung2");
+        System.out.println("");
         int h = 0;
         while (h <= 10) {
             System.out.println(h);
             ++h;
         }
-
+        System.out.println("");
+        System.out.println("Übung 3");
+        System.out.println("");
         for (int i = 0; i <= 10; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
@@ -34,11 +40,12 @@ public class CountTheNumbers {
                 System.out.println("Hello");
             }
         }
+        System.out.println("");
+        System.out.println("Übung 4");
+        System.out.println("");
+        for (int u = -10; u <= 10; u++) {
 
-
-            for (int u = 10; u >= -10; u--) {
-
-                System.out.println(u);
-            }
+            System.out.println(u);
         }
     }
+}
