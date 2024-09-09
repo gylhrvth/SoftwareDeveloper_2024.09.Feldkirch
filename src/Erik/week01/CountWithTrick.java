c27511e3a70b59699e7b28d9437f8ca5f699f3f3
Erik <silverbulllp@gmail.com>
2024-09-09 12:29:57 +0200

    Count With Trick

--- src/Erik/week01/CountWithTrick.java
+++ src/Erik/week01/CountWithTrick.java
@@ -3,7 +3,7 @@ package Erik.week01;
 public class CountWithTrick {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 11; i += 2) {
+        for (int i = 0; i <= 10; i += 2) {
             System.out.println(i);
 
         }
