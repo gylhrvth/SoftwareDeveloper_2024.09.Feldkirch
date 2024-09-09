715dfcf233c97ad9a025baea817a6a1915d18e22
Erik <silverbulllp@gmail.com>
2024-09-09 13:12:14 +0200

    Rewind

---
+++ src/Erik/week01/Rewind.java
@@ -0,0 +1,10 @@
+package Erik.week01;
+
+public class Rewind {
+    public static void main(String[] args) {
+
+        for (int i = 10; i != -11; i--) {
+            System.out.println(i);
+        }
+    }
+}
