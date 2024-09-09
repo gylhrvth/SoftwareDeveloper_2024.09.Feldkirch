2c45d104a6a1c22db93c0f1325cd12e439472fc3
Aylin <topcuazlin98@gmail.com>
2024-09-09 11:05:12 +0200

    printName

---
+++ src/Aylin/week01/printName.java
@@ -0,0 +1,12 @@
+package Aylin.week01;
+
+public class printName {
+    public static void main(String[] args) {
+        System.out.println("     /--\\     \\-\\    /-/   |-|        |-|   |-\\ \\    |-|");
+        System.out.println("    / /\\ \\     \\ \\  / /    | |        | |   | |\\ \\   | |");
+        System.out.println("   / /  \\ \\     \\ \\/ /     | |        | |   | | \\ \\  | |");
+        System.out.println("  / /    \\ \\     \\  /      | |        | |   | |  \\ \\ | |");
+        System.out.println(" /  =======  \\   | |       | |_____   | |   | |   \\ \\| |");
+        System.out.println("/_/         \\_\\  |_|       |_______|  |_|   |_|    \\___|");
+    }
+}
