a62eba8aec0ac546049cb3ba59c848189a4359d0
Akif <akifuenalan@gmail.com>
2024-09-09 15:01:00 +0200

    Count with numbers fixed

--- src/Akif_Malik/week01/Zählen2.java
+++ src/Akif_Malik/week01/Zählen2.java
@@ -1,9 +1,11 @@
 package Akif_Malik.week01;
 
+import java.util.Arrays;
+
 public class Zählen2 {
     public static void main(String[] args) {
-        for (int i = 10; i != -11; i--) {
-            System.out.println(i);
+        for (int i = 10; i >= -10; i-=2) {
+            System.out.print(i);
         }
 
     }
