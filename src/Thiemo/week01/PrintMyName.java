26321b7cd9ba874bbf9352c0672ae0da27fd2425
Thiemo <thiemo.hofacker@gmail.com>
2024-09-09 11:28:02 +0200

    Aufgabe: Print A

---
+++ src/Thiemo/week01/PrintMyName.java
@@ -0,0 +1,16 @@
+package Thiemo.week01;
+
+public class PrintMyName {
+    public static void main(String[] args) {
+
+        System.out.println("" +
+                " _______ _      _                      \n" +
+                " |__   __| |   (_)                     \n" +
+                "    | |  | |__  _  ___ _ __ ___   ___  \n" +
+                "    | |  | '_ \\| |/ _ \\ '_ ` _ \\ / _ \\ \n" +
+                "    | |  | | | | |  __/ | | | | | (_) |\n" +
+                "    |_|  |_| |_|_|\\___|_| |_| |_|\\___/ \n" +
+                "                                       ");
+
+
+}
