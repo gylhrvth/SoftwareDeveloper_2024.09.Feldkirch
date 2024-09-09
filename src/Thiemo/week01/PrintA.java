010571790367bc89f0e73aae19a4a5e6a72ce717
Thiemo <thiemo.hofacker@gmail.com>
2024-09-09 11:31:03 +0200

    Aufgabe: My Name

--- src/Thiemo/week01/PrintA.java
+++ src/Thiemo/week01/PrintA.java
@@ -4,8 +4,8 @@ public class PrintA {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println();
-        System.out.println("  " +
-                "   /--\\\n" +
+        System.out.println("     " +
+                "/--\\\n" +
                 "    / /\\ \\\n" +
                 "   / /  \\ \\\n" +
                 "  / /    \\ \\\n" +
