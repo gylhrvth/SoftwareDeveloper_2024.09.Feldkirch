ad1c9df5b5b9be41dd2ae52434132db7d1b6b7de
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-09 11:30:30 +0200

    Namen commit

---
+++ src/Furkan/week01/PrintF.java
@@ -0,0 +1,32 @@
+package Furkan.week01;
+
+public class PrintF{
+    public static void main(String[] args) {
+
+        System.out.println("|        \\                   |  \\                         \n" +
+                           "| $$$$$$$$__    __   ______  | $$   __  ______   _______  \n" +
+                           "| $$__   |  \\  |  \\ /      \\ | $$  /  \\|      \\ |       \\ \n" +
+                           "| $$  \\  | $$  | $$|  $$$$$$\\| $$_/  $$ \\$$$$$$\\| $$$$$$$\\\n" +
+                           "| $$$$$  | $$  | $$| $$   \\$$| $$   $$ /      $$| $$  | $$\n" +
+                           "| $$     | $$__/ $$| $$      | $$$$$$\\|  $$$$$$$| $$  | $$\n" +
+                           "| $$      \\$$    $$| $$      | $$  \\$$\\\\$$    $$| $$  | $$\n" +
+                           " \\$$       \\$$$$$$  \\$$       \\$$   \\$$ \\$$$$$$$ \\$$   \\$$");
+
+        System.out.println();
+        System.out.println("FFFFFFFFFFFFFFFFFF");
+        System.out.println("F");
+        System.out.println("F");
+        System.out.println("F");
+        System.out.println("F");
+        System.out.println("F");
+        System.out.println("F");
+        System.out.println("FFFFFFFFFFFFFFFFFF     uu           uu    rrrrrrrrr");
+        System.out.println("F                      uu           uu   rr        rr");
+        System.out.println("F                      uu           uu   rr");
+        System.out.println("F                      uu           uu   rr");
+        System.out.println("F                      uu           uu   rr");
+        System.out.println("F                      uu           uu   rr");
+        System.out.println("F                       uuuuuuuuuuuu     rr");
+
+    }
+}
