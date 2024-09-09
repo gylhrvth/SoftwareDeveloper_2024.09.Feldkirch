4dd4c17731ebd5736da1a870f2b783e3e190c17c
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-09 13:16:19 +0200

    Aufgabe 2 Zählen

---
+++ src/Furkan/week01/AufgabeZählen2.java
@@ -0,0 +1,20 @@
+package Furkan.week01;
+
+public class AufgabeZählen2 {
+    public static void main(String[] args) {
+        int countloops =10;
+        System.out.println(countloops);
+        System.out.println("count loops is " +10);
+
+        for (int i=0; i <10; ++i) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        int i= 1;
+        while (i <10) {
+            System.out.println(i);
+            ++i;
+        }
+    }
+}
