f0d23c9de9ed9a3a9ae780fecc07edfe966d51d2
Erik <silverbulllp@gmail.com>
2024-09-10 11:21:25 +0200

    PrintCharacters Task 1-3

--- src/Erik/week01/PrintCharacters.java
+++ src/Erik/week01/PrintCharacters.java
@@ -2,25 +2,55 @@ package Erik.week01;
 
 public class PrintCharacters {
     public static void main(String[] args) {
-        printChars("x", 10);
-        System.out.println(" ");
-        System.out.println("=============");
+        printChars("x ", 10);
+        System.out.println();
+        System.out.println("============================"); //Ende erste Aufgabe
         printSquare("x", 10);
+        System.out.println("============================"); //Ende zweite Aufgabe
+        printRectangle("A ", 10, 3);
+        System.out.println("============================"); //Ende dritte Aufgabe
+        printTrinangle1("X ", 4);
+
+
+
     }
 
+    //Aufgabe 1 Print Characters
     public static void printChars(String text, int count) {
         for (int i = 0; i < count; i++) {
             System.out.print(text);
         }
     }
 
+    //Aufgabe 2 Print Square
     public static void printSquare(String text, int count) {
-        for (int i = 0; i < count; i++) {
-            System.out.print(text);
-            System.out.println(" ");
-
+        for (int row = 0; row < count; row++) {
+            for (int i = 0; i < count; i++) {
+                System.out.print("x ");
+            }
+            System.out.println();
         }
     }
+
+    //Aufgabe 3 Print Rectangle
+    public static void printRectangle(String text, int countColumns, int countRows) {
+        for (int row = 0; row < countRows; row++) {
+            for (int col = 0; col < countColumns; col++) {
+                System.out.print(text);
+            }
+            System.out.println();
+        }
+    }
+
+    //Aufgabe 4 Print Trinangle 1
+    public static void printTrinangle1(String text, int count) {
+        for (int row = 0; row < count; row++) {
+            for (int i = 4; i < count; i++) {
+                System.out.print("x ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
-
-
