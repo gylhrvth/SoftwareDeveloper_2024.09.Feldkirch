52bcc3258c9c4bbfd70f0f146b1a65b124e5a4ad
Akif <akifuenalan@gmail.com>
2024-09-10 12:03:30 +0200

    Printing Trinangle 2 (fixed)

---
+++ src/Akif_Malik/week01/PrintTrinangle2.java
@@ -0,0 +1,21 @@
+package Akif_Malik.week01;
+
+public class PrintTrinangle2 {
+    public static void main(String[] args) {
+        printTriangleTopLeft("x ", 5);
+
+    }
+
+
+
+
+
+    public static void printTriangleTopLeft(String text, int count){
+        for (int row = 0; row < count ; row++) {
+            for (int column = 0; column < count-row  ; column++) {
+                System.out.print(text);
+            }
+            System.out.println();
+        }
+    }
+}
