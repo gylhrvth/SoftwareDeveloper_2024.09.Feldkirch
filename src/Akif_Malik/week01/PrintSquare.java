2e251710dcaca6d7855a831be6fc1b3f2a47ef1f
Akif <akifuenalan@gmail.com>
2024-09-10 08:52:38 +0200

    Printing Square

---
+++ src/Akif_Malik/week01/PrintSquare.java
@@ -0,0 +1,26 @@
+package Akif_Malik.week01;
+
+public class PrintSquare {
+    public static void main(String[] args) {
+        printSquare("x", 10);
+    }
+
+
+
+
+    public static void printSquare(String text, int count){
+        for (int i = 0; i <= count; i++) {
+            System.out.print(text);
+            System.out.print(text);
+            System.out.print(text);
+            System.out.print(text);
+            System.out.print(text);
+            System.out.print(text);
+            System.out.print(text);
+            System.out.print(text);
+            System.out.print(text);
+            System.out.println(text);
+        }
+
+    }
+}
