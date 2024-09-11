5e10747bb684efcc22369d053c1ace5745d11db7
Akif <akifuenalan@gmail.com>
2024-09-11 13:32:07 +0200

    print slashes (both)

---
+++ src/Akif_Malik/week01/printSlash.java
@@ -0,0 +1,41 @@
+package Akif_Malik.week01;
+
+public class printSlash {
+    public static void main(String[] args) {
+        printslash("x ", 5, false);
+        System.out.println(" .-. .-. .-. .-. .-. .-. .-. ");
+        System.out.println();
+        printslash("y ", 5, true);
+
+    }
+
+    public static void printslash(String text, int maxRep, boolean isBackslash) {
+        if (isBackslash){
+            printBackSlash(text, maxRep);
+        } else {
+            printForwardSlash(text, maxRep);
+        }
+    }
+
+    public static void printBackSlash(String text, int maxRep) {
+        for (int row = 0; row < maxRep ; row++) {
+            for (int column = 0; column < row; column++) {
+                System.out.print(". ");
+            }
+            System.out.println(text);
+
+        }
+    }
+
+    public static void printForwardSlash(String text, int maxRep) {
+        for (int row = 0; row < maxRep ; row++) {
+            for (int column = 0; column < maxRep-row - 1  ; column++) {
+                System.out.print(". ");
+            }
+            System.out.println(text);
+        }
+        System.out.println();
+
+    }
+
+}
