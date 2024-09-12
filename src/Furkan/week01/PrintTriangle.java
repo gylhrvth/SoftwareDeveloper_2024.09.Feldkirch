1b598872ca3760e07b5a6257b2a9ad818bd33180
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-12 11:46:44 +0200

    PrintSlash

---
+++ src/Furkan/week01/PrintTriangle.java
@@ -0,0 +1,24 @@
+package Furkan.week01;
+
+public class PrintTriangle {
+    public static void main(String[] args) {
+        printTriangle("x", 3);
+    }
+
+    public static void printTriangle(String text, int size) {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < 2 * size - 1; col++) {
+                if (row + col == size - 1) {
+                    System.out.print("A ");
+                } else if (col == row + size - 1) {
+                    System.out.print("B ");
+                } else if (row == size -1){
+                    System.out.print("C ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
