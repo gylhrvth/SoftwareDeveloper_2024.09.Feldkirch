fedbb59e8564d2d1bff3d3ac0cf73af8086a5b10
Akif <akifuenalan@gmail.com>
2024-09-12 14:37:18 +0200

    Print Rhombus finished (fixed)

--- src/Akif_Malik/week01/PrintRhombus.java
+++ src/Akif_Malik/week01/PrintRhombus.java
@@ -7,25 +7,25 @@ public class PrintRhombus {
     }
 
 
-    public static void printRhombus(String text, int size) {
-        if(size % 2 == 0) {
-            size--;
-        }for(int row = 0; row < size; row++){
-            for(int column = 0; column < size; column++){
-                if (row + size /2 == column) {
+    public static void printRhombus(String text, int count) {
+        if (count % 2 == 0) {
+            count--;
+        }
+        for (int row = 0; row < count; row++) {
+            for (int column = 0; column < count; column++) {
+                if (row + count / 2 == column) {
                     System.out.print(text);
-                }else if(column + size / 2 == row) {
+                } else if (column + count / 2 == row) {
                     System.out.print(text);
-                }else if(row + column == size / 2) {
+                } else if (row + column == count / 2) {
                     System.out.print(text);
-                }else if(row + column - (size / 2) == size -1) {
+                } else if (row + column - (count / 2) == count - 1) {
                     System.out.print(text);
-                }else{
+                } else {
                     System.out.print(". ");
                 }
             }
             System.out.println();
         }
-        System.out.println();
     }
 }
