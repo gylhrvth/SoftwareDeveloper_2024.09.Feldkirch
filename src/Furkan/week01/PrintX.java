5dd83029e275a8d7a11af4fbda76ee8ad76febfc
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-13 09:13:36 +0200

    PrintX

--- src/Furkan/week01/PrintX.java
+++ src/Furkan/week01/PrintX.java
@@ -7,11 +7,20 @@ public class PrintX {
     }
 
 
-    public static void printX(String text, int size){
-        for (int row = 0; row < size; row++){
-            for (int col = 0; col< 2*size; col++){
+    public static void printX(String text, int size) {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < 2 * size; col++) {
+                if (col == row) {
+                    System.out.print(text);
+                } else if (row + col == size -1){
+                    System.out.print(text);
+
+                }else{
+                    System.out.print(".");
+                }
 
             }
+            System.out.println();
         }
 
     }
