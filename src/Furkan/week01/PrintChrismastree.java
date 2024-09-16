227f0b6599bf2d676651e77e21a55df2e78716de
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-16 10:23:58 +0200

    PrintChrismastree

--- src/Furkan/week01/PrintChrismastree.java
+++ src/Furkan/week01/PrintChrismastree.java
@@ -10,12 +10,12 @@ public class PrintChrismastree {
     public static void prinChristmastree(int size) {
 
         for (int row = 0; row < size; row++) {
-            for (int col = 0; col < 2* size-1 ; col++) {
-                if (row + col == size -1) {
+            for (int col = 0; col < 2 * size - 1; col++) {
+                if (row + col == size - 1) {
                     System.out.print("A");
-                } else if (col == row + size-1) {
+                } else if (col == row + size - 1) {
                     System.out.print("B");
-                } else if (row +col == 3*(size-1)) {
+                } else if (row == size - 1) {
                     System.out.print("C");
                 } else {
                     System.out.print(".");
@@ -23,6 +23,15 @@ public class PrintChrismastree {
             }
             System.out.println();
         }
+        for (int leaves = 0; leaves < size; leaves++) {
+            System.out.print("0 ");
+        }
+        System.out.println();
+
+        for (int trunk = 0; trunk <= size - 1; trunk++) {
+            System.out.print(" ");
+        }
+        System.out.println("");
     }
 
 
