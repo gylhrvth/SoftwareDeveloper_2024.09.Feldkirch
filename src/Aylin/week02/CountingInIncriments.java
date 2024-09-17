cbf4f0d3e331881ce7eba5fe4d3a54c116c23a98
Aylin Topcu <topcuaylin98@gmail.com>
2024-09-17 14:27:46 +0200

    Counting in incriments

--- src/Aylin/week02/CountingInIncriments.java
+++ src/Aylin/week02/CountingInIncriments.java
@@ -3,21 +3,17 @@ package Aylin.week02;
 public class CountingInIncriments {
     public static void main(String[] args) {
         float start = 0;
-        int ende = 100;
+        float ende = 100.0f;
         float komma = 0.1f;
 
         for (start = komma; start <= ende ; start+= komma) {
             System.out.printf("%-2.1f \n", start);
         }
         System.out.println();
-        System.out.println("mit Double");
+        System.out.println("2. Variante");
 
-        double begin = 0;
-        int end = 100;
-        double decimal = 0.1f;
-
-        for (begin = decimal; begin <= end ; begin+= decimal) {
-            System.out.printf("%-1.1f \n", begin);
+        for (int sum = 0; sum <= 1000; sum++) {
+            System.out.println(sum / 10.0f);
         }
     }
 }
