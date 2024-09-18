761a62be9ac89e0e547ea720f98c7de193479075
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-18 10:45:27 +0200

    dividieren

--- src/Furkan/week02/TeilenHerrschen.java
+++ src/Furkan/week02/TeilenHerrschen.java
@@ -2,6 +2,27 @@ package Furkan.week02;
 
 public class TeilenHerrschen {
     public static void main(String[] args) {
+        int start = 0;
+        int end = 20;
+        int num = 5;
+        double d = 5.0;
+
+       // System.out.println();
+        dividebynummber(start, end, num, d);
+        //System.out.println();
+
+
+    }
+
+    public static void dividebynummber(int start, int end, int num, double d) {
+        for (int i= start = 0; start <= end; start++) {
+            System.out.printf("%d / 5= ", start);
+            System.out.println(start / num);
+            System.out.printf("%d / 5.0= ", start);
+            System.out.println(start /d);
+
+        }
 
     }
 }
+
