3b32bea19b71956b61e9a197254096ae466b3c4c
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-18 10:45:13 +0200

    Faktorial2

--- src/Furkan/week02/Faktorial2.java
+++ src/Furkan/week02/Faktorial2.java
@@ -25,7 +25,7 @@ public class Faktorial2 {
         //Aufgabe 2 mit verschiedene Formatstangen
 
     public static void countbyTenth(int start, int end) {
-        for (float i = 0; i <= 100; i += 0.1) {
+        for (float i = 0; i <= end; i += 0.1) {
             System.out.printf("Result 1 digit =" + "%5.1f\n", i);
             System.out.printf("Result 2 digit =" + "%-6.2f\n", i);
 
