8cf1e698ba65f99663506426fbed790fcb99dfa6
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-18 15:26:35 +0200

    PrintRhombus

--- src/Furkan/week01/PrintRhombus.java
+++ src/Furkan/week01/PrintRhombus.java
@@ -10,13 +10,13 @@ public class PrintRhombus {
         for (int row = 0; row < 2* size-1  ; row++) {
             for (int col = 0; col < 2 * size-1 ; col++) {
                 if (row + col == size-1) {
-                    System.out.print("A");
+                    System.out.print(text);
                 } else if (col == row + size -1) {
-                    System.out.print("B");
+                    System.out.print(text);
                 } else if (row + col ==3* (size -1) ){
-                    System.out.print("C");
+                    System.out.print(text);
                 }else if (col == row -(size-1) ){
-                    System.out.print("D");
+                    System.out.print(text);
                 }
 
 
