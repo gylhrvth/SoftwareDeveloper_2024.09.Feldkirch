07b7090b3b49c31432037273b52cb99b98b54610
Thiemo <thiemo.hofacker@gmail.com>
2024-09-18 15:30:05 +0200

    Benutzeingabe versuchen

---
+++ src/Thiemo/week02/Benutzereingabe.java
@@ -0,0 +1,10 @@
+package Thiemo.week02;
+
+import java.util.Scanner;
+
+public class Benutzereingabe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
