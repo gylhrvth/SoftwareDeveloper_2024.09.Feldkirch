026c523bc4a8753a56093e25175fe017094f5737
Gyula Horvath <gylhrvth@gmail.com>
2024-09-18 11:14:06 +0200

    Leibnitz Serie (schrittweise Aufbau)

---
+++ src/Gyula/week02/Leibnitz.java
@@ -0,0 +1,44 @@
+package Gyula.week02;
+
+public class Leibnitz {
+    public static void main(String[] args) {
+        //calculatePi();
+        //calculatePiV2();
+        calculatePiV3();
+    }
+
+    public static void calculatePi(){
+        double sum = 0.0;
+        for (int i = 1; i < 10000; i += 2) {
+            int vorzeichnen = 1;
+            if (i % 4 == 3){
+                vorzeichnen = -1;
+            }
+            sum += vorzeichnen * 4.0 / i;
+            System.out.println(sum);
+        }
+        System.out.println(sum);
+    }
+
+    public static void calculatePiV2(){
+        double sum = 0.0;
+        for (int i = 1; i < 100000; i += 4) {
+            sum += 4.0 / i;
+            sum -= 4.0/(i + 2);
+        }
+        System.out.println(sum);
+    }
+
+
+    public static void calculatePiV3(){
+        double sumMax = 4.0;
+        double sumMin = 0.0;
+        int i = 1;
+        while (sumMax - sumMin > 0.00001) {
+            sumMax = sumMin + 4.0 / i;
+            sumMin = sumMax - 4.0/(i + 2);
+            i += 4;
+        }
+        System.out.println(sumMax + " " + sumMin);
+    }
+}
