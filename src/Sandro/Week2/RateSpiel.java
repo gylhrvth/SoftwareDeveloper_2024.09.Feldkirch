d01d8b88831d3f18a5b67f90f427b61ea67f81a2
ossdraboon <ossdraboon@gmail.com>
2024-09-19 15:30:04 +0200

    Rehearsal Week2 Wiederholung Format String Wiederholung Methoden, rückgabewert, etc

---
+++ src/Sandro/Week2/RateSpiel.java
@@ -0,0 +1,27 @@
+package Sandro.Week2;
+
+import java.util.Random;
+
+public class RateSpiel {
+    public static void main(String[] args) {
+        //MainBlock
+
+        //Todo: PrintText - Funktion
+        //Todo: Scanner (String und int) - Funktion
+        //Todo: range 1-100 (abfrage) - Funktion
+        //Todo: Random zahl (1-100) Rando.nextInt(). - Variable
+        //Todo: Logik (vergleich von SpielerZahl und RandomZahl) - If(Else)
+        //Todo: Logik while(true or false) - boolean
+
+        //Todo: Counter (wie oft spielen), Farben zeigen nähe an, range ist auch mit counter verküpft,
+        //Todo: Personalieserte anrede des spielers (Name),
+        //Todo: Joker (random oder aktivierbar)
+
+
+
+    }
+
+    //Funktionen, Methoden
+
+
+}
