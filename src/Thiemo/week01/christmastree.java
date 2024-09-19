6b1357f411268133453435c70f7d71bda25f806b
Thiemo <thiemo.hofacker@gmail.com>
2024-09-19 21:51:00 +0200

    added colors

--- src/Thiemo/week01/christmastree.java
+++ src/Thiemo/week01/christmastree.java
@@ -3,17 +3,22 @@ package Thiemo.week01;
 public class christmastree {
     public static void main(String[] args) {
         System.out.println();
-        christmastree(20);
+        christmastree(10);
         System.out.println();
+
     }
 
     public static void christmastree(int count) {
+
+        String leafes = "\u001B[32m*\u001B[0m";
+        String trunk = "\u001B[33m+\u001B[0m";
+
         for (int rowleafes = 0; rowleafes < count; rowleafes++) {
             for (int spacebar = 0; spacebar < count - rowleafes - 1; spacebar++) {
                 System.out.print(".");
             }
             for (int columnleafes = 0; columnleafes <= 2 * rowleafes; ++columnleafes) {
-                System.out.print("*");
+                System.out.print(leafes);
             }
             System.out.println();
         }
@@ -29,7 +34,7 @@ public class christmastree {
 
                 for (int columnTrunk = 0; columnTrunk < count / 2 ;  columnTrunk++) {
 
-                    System.out.print("+");
+                    System.out.print(trunk);
                 }
 
                 System.out.println();
