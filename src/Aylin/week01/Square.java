c2c6532832c611c65de77770286e7e29466f8bca
Aylin Topcu <topcuaylin98@gmail.com>
2024-09-23 13:36:58 +0200

    Square new

--- src/Aylin/week01/Square.java
+++ src/Aylin/week01/Square.java
@@ -4,14 +4,14 @@ public class Square {
     public static void main(String[] args) {
         System.out.println("Print Empty Square");
         System.out.println();
-        printEmptySquare(10);
+        printEmptySquare("c ", 10);
     }
 
-    public static void printEmptySquare(int breite) {
+    public static void printEmptySquare(String text, int breite) {
         for (int zeile = 0; zeile < breite; zeile++) {
             for (int spalte = 0; spalte < breite; spalte++) {
                 if (spalte == 0 || zeile == 0 || spalte == breite - 1 ||  zeile == breite - 1) {
-                    System.out.print("* ");
+                    System.out.print(text);
                 } else {
                     System.out.print(". ");
                 }
