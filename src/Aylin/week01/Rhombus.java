125b2991435f991ba6588565d7a51ce9166bf4f2
Aylin Topcu <topcuaylin98@gmail.com>
2024-09-23 13:36:33 +0200

    Rhombus new

--- src/Aylin/week01/Rhombus.java
+++ src/Aylin/week01/Rhombus.java
@@ -4,23 +4,23 @@ public class Rhombus {
     public static void main(String[] args) {
         System.out.println("Print Rhombus ");
         System.out.println();
-        printRhombus(20);
+        printRhombus("e", 20);
     }
 
-    public static void printRhombus(int count){
+    public static void printRhombus(String text, int count){
         if(count % 2 == 0) {
             count--;
         }
         for(int row = 0; row < count; row++){
             for(int column = 0; column < count; column++){
                 if (row + count /2 == column) {
-                    System.out.print("a");
+                    System.out.print(text);
                 }else if(column + count / 2 == row) {
-                    System.out.print("a");
+                    System.out.print(text);
                 }else if(row + column == count / 2) {
-                    System.out.print("a");
+                    System.out.print(text);
                 }else if(row + column - (count / 2) == count -1) {
-                    System.out.print("a");
+                    System.out.print(text);
                 }else{
                     System.out.print(".");
                 }
