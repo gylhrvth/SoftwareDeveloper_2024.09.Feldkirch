f42391926f74e10c6962b2b08c9bb48d43a31f36
Erik <silverbulllp@gmail.com>
2024-09-23 11:53:09 +0200

    with little changes

--- src/Erik/week02/RateSpiel.java
+++ src/Erik/week02/RateSpiel.java
@@ -5,45 +5,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RateSpiel {
+    public static Random randomGenerator = new Random();          // Random Number Generator über dem Main Block muss nur einmal definiert werden und kann überall verwendet werden
+    public static Scanner sc = new Scanner(System.in);            // Scanner wird einmal über dem Main Block initialisiert und kann überall verwendet werden. Muss man nicht mehrmals neu schreiben
+
     public static void main(String[] args) {
-        int number = 0;
-        String name = "";
 
-        Random rand = new Random();
-        int guessingNumber = rand.nextInt(0, 5);
+        int guessingNumber = randomGenerator.nextInt(0, 5);
 
-        System.out.println(guessingNumber);
-        name = readString("What's your Name?");
+      //  System.out.println(guessingNumber);
+        String name = readString("What's your Name?");
         System.out.println("Hello " + name + " Welcome to the Ratespiel!");
-        number = giveNumber("give me a number between 0-5", 0, 5, rand.nextInt());
-        GameLogicThroughNumber("", number, guessingNumber);
+        int number = giveNumber("give me a number between 0-5", 0, 5, randomGenerator.nextInt());
+        GameLogicThroughNumber("", guessingNumber);
 
-//        boolean test = GameLogicThroughNumber("test", 4,3);
-//        System.out.println(test);
 
     }
 
     public static String readString(String UserInfo) {
         System.out.println(UserInfo);
-        String text = "";
+        String text = "";               // Unnötige Initialisierung
         Scanner sc = new Scanner(System.in);
         text = sc.nextLine();
         return text;
     }
 
-    public static boolean GameLogicThroughNumber(String msg, int number, int guessingNumber) {
-        Scanner sc = new Scanner(System.in);
+    public static boolean GameLogicThroughNumber(String msg, int guessingNumber) {
         boolean win = false;
         int versuch = 0;
         int maxVersuche = 3;
 
-        while (versuch < maxVersuche && win == false) {
+        while (versuch < maxVersuche && !win) {
 
-            number = sc.nextInt();
-            versuch ++;
+            int number = sc.nextInt();
+            versuch++;
 
             if (number == guessingNumber) {
-                System.out.println("Congratulation! You won the Game!");
                 win = true;
             } else if (number < guessingNumber) {
                 System.out.println("Your guess is to low! Try higher \n" + "Give me another Number");
@@ -52,18 +48,19 @@ public class RateSpiel {
             }
         }
 
-            if (win == false){
-                System.out.println("You loose! \n" + "Your guessing Number would have been " + guessingNumber);
-            }
-        sc.close();
+        if (!win) {
+            System.out.println("You loose! \n" + "Your guessing Number would have been " + guessingNumber);
+        } else {
+            System.out.println("Congratulation! You won the Game!");
+        }
         return win;
+
     }
 
 
     public static int giveNumber(String msg, int minValue, int maxValue, int guessingNumber) {
 
         int number = 0;
-        Scanner sc = new Scanner(System.in);
         boolean validNumber = false;
 
         while (validNumber == false)
@@ -90,15 +87,15 @@ public class RateSpiel {
 
 
 
-
-//        Methoden/Funktion's Tests:
-//
-//        boolean test = infoThroughNumber("test", 3,3);
-//        System.out.println(test);
-//
-//        int giveNumber = giveNumber("test", 1, 3, 2);
-//        System.out.println(giveNumber);
 /*
+        Methoden/Funktion's Tests:
+
+        boolean test = infoThroughNumber("test", 3,3);
+        System.out.println(test);
+
+        int giveNumber = giveNumber("test", 1, 3, 2);
+        System.out.println(giveNumber);
+
 while (run == false) {
             if (run = false){
                 System.out.println("give me another number");
@@ -106,4 +103,7 @@ while (run == false) {
                 System.out.println("Congratulation! You won the Game!");
                 run = true;
             }
+
+        boolean test = GameLogicThroughNumber("test", 4,3);
+        System.out.println(test);
  */
