ef6634019199e974148d44b8dc843e15cc97fc48
Thiemo <thiemo.hofacker@gmail.com>
2024-09-24 21:49:36 +0200

    .

--- src/Thiemo/week01/rhombus2.java
+++ src/Thiemo/week01/rhombus2.java
@@ -1,26 +1,27 @@
 package Thiemo.week01;
 
-public class rhombus2 {    public static void main(String[] args) {
-    System.out.println("Print Rhombus ");
-    System.out.println();
-    printRhombus("e", 9);
-}
+public class rhombus2 {
+    public static void main(String[] args) {
+        System.out.println("Print Rhombus ");
+        System.out.println();
+        printRhombus("e", 9);
+    }
 
-    public static void printRhombus(String text, int count){
-        if(count % 2 == 0) {
+    public static void printRhombus(String text, int count) {
+        if (count % 2 == 0) {
             count--;
         }
-        for(int row = 0; row < count; row++){
-            for(int column = 0; column < count; column++){
-                if (row + count /2 == column) {
+        for (int row = 0; row < count; row++) {
+            for (int column = 0; column < count; column++) {
+                if (row + count / 2 == column) {
                     System.out.print(row + column);
-                }else if(column + count / 2 == row) {
+                } else if (column + count / 2 == row) {
                     System.out.print(row + column);
-                }else if(row + column == count / 2) {
+                } else if (row + column == count / 2) {
                     System.out.print(row + column);
-                }else if(row + column - (count / 2) == count -1) {
+                } else if (row + column - (count / 2) == count - 1) {
                     System.out.print(row + column);
-                }else{
+                } else {
                     System.out.print(".");
                 }
             }
