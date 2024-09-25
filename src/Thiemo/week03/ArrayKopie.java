fefedf9d62044871ce7180cc462f81cb8de6eead
Thiemo <thiemo.hofacker@gmail.com>
2024-09-25 10:36:16 +0200

    added some unknown stuff

--- src/Thiemo/week03/ArrayKopie.java
+++ src/Thiemo/week03/ArrayKopie.java
@@ -7,18 +7,31 @@ import java.util.Arrays;
 public class ArrayKopie {
     public static void main(String[] args) {
 
+//            int array[] = {6, 7, 8, 9, 10};
+//
+//            Arrays.fill(array, 5);                 //overwrite the array with 5 instead of the given numbers
+//
+//            System.out.println(Arrays.toString(array));
+
 
         int size = ConsoleTools.readNumber("Array Size:", 0, 999);
-        int[] array = createAndFillArray(size);
-        System.out.println(Arrays.toString(array));
+        int[] array1 = createAndFillArray(size);
+        System.out.println(Arrays.toString(array1));
         System.out.println();
 
         int[] array2 = createAndFillArrayBackwards(size);
+
         System.out.println(Arrays.toString(array2));
         System.out.println();
-        int[] arraykopie = makeACopy(array);
+        array2[0]=50;
+        array2[1]=25;
+        int[] arraykopie = makeACopy(array1);
         int[] arraykopie2 = makeACopy(array2);
-        System.out.println(Arrays.toString(arraykopie)+"\n"+"\n"+(Arrays.toString(arraykopie2)));
+
+        System.out.println(Arrays.toString(arraykopie) + "\n" +"\n" +(Arrays.toString(arraykopie2))+"\n");
+
+        int[]funarray = {100,50,25};
+        System.out.println(Arrays.toString(funarray));
     }
 
     public static int[] createAndFillArray(int size) {
@@ -35,18 +48,17 @@ public class ArrayKopie {
 //            size --;
 //            result[i] = size;
             result[i] = /* size--; size - i;*/result.length - i;
-
         }
         return result;
 
     }
 
     public static int[] makeACopy(int[] originalarr1) {
-
         int[] copy = new int[originalarr1.length];
         for (int i = 0; i < originalarr1.length; i++) {
             copy[i] = originalarr1[i];
         }
+//        System.arraycopy(originalarr1, 0, copy, 0, originalarr1.length);
         return copy;
 
     }
