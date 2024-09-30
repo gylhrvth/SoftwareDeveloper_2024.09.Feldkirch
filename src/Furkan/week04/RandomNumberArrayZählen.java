3a1a998c36d2ef2f4297b610af9095dc0ed96da0
Furkan <furkan.bagriacik4646@gmail.com>
2024-09-30 14:40:21 +0200

    ArrayAufgaben mit Random zahlen erte über 30 zählen

---
+++ src/Furkan/week04/RandomNumberArrayZählen.java
@@ -0,0 +1,39 @@
+package Furkan.week04;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomNumberArrayZählen {
+
+    static Random rand = new Random();
+
+    public static void main(String[] args) {
+        int size = 10;
+        int[] arrayRandom = createArrayRandom(size);
+        System.out.println(Arrays.toString(arrayRandom));
+        int count =countNumbersOver(arrayRandom);
+        System.out.println("Die Werte über 30 sind + " + count);
+
+
+    }
+
+    public static int[] createArrayRandom(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(0, 101);
+        }
+
+        return arr;
+    }
+
+    public static int countNumbersOver(int[] arr) {
+        int count = 0;
+        for (int number : arr) {
+            if (number > 30) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
