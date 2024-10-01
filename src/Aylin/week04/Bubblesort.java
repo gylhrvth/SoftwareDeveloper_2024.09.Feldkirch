2da07fa69ee8e470ffafd8f41aabca6929292465
Aylin Topcu <topcuaylin98@gmail.com>
2024-10-01 10:49:32 +0200

    herum experimentiert

--- src/Aylin/week04/Bubblesort.java
+++ src/Aylin/week04/Bubblesort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Bubblesort {
     public static void main(String[] args) {
         int [] arr = Aylin.week03.RandomNumberArray.randomNumber(20);
+        System.out.println(Arrays.toString(arr));
         bubblesort(arr);
         bubblesortBackwards(arr);
     }
@@ -28,11 +29,11 @@ public class Bubblesort {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if(arr[j] < arr[j + 1]){
-//                    System.out.println(Arrays.toString(arr));
+                    System.out.println(Arrays.toString(arr));
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-//                    System.out.println(j + " und " + (j + 1) + " müssen getauscht werden");
+                    System.out.println(j + " und " + (j + 1) + " müssen getauscht werden");
                 }
             }
         }
