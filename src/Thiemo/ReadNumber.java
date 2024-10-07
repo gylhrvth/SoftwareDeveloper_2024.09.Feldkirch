2212bcd3924c802ed593409c7ce0a31e6687f9d7
Thiemo <thiemo.hofacker@gmail.com>
2024-10-07 19:05:15 +0200

    Tool zum Zahl einlesen

---
+++ src/Thiemo/ReadNumber.java
@@ -0,0 +1,37 @@
+package Thiemo;
+
+import java.util.Scanner;
+
+public class ReadNumber {
+
+    public static int readNumber(String userinfo, int minValue, int maxValue) {
+
+        int number = 0;
+        Scanner sc = new Scanner(System.in);
+        boolean richtigeEingabe = false;
+        // while (richtinge eingabe == false),
+        while (!richtigeEingabe) {
+            try {
+                System.out.println(userinfo);
+                String hilfsText = sc.nextLine();
+                number = Integer.parseInt(hilfsText);
+//                number = sc.nextInt();
+                if (number >= minValue && number <= maxValue) {
+                    richtigeEingabe = true;
+                } else {
+                    System.out.println("Versuchs von " + minValue+ " bis " + maxValue + "\n"/*+userinfo*/);
+                }
+
+            }catch (NumberFormatException nfe)
+//            catch (InputMismatchException ime)
+            {
+                System.out.println("Fail! Maybe try a Number/Something else? " + " \n " +/*ime*/ "\n"/*+userinfo*/);
+//                sc.nextLine();
+            }
+        }
+        return number;
+    }
+
+}
+
+
