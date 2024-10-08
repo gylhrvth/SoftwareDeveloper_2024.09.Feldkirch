45807de3681e6ff0601fce57c4d4502b0a4aa608
Furkan <furkan.bagriacik4646@gmail.com>
2024-10-08 15:23:26 +0200

    ConwaysGameofLife erster versuch

---
+++ src/Furkan/week05/ConwaysGameOfLife.java
@@ -0,0 +1,61 @@
+package Furkan.week05;
+
+/*
+ToDo:
+1: Repräsentation der Daten
+2: Print
+3: Anzahl Nachbarn
+4: Generieren neue Generationen
+ */
+
+
+import Sandro.Colors;
+
+public class ConwaysGameOfLife {
+
+    public static final int[][] GLEITER = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
+
+
+    public static void main(String[] args) {
+    int [][] field = GLEITER;
+    printField(field);
+
+
+
+    }
+
+
+    public static void printField(int [][] field){
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length; j++) {
+                if (field[i][j] == 1){
+                    System.out.print(Colors.BACKGROUND_COLORS[2] + "  " + Colors.RESET);
+                }else {
+                    System.out.print(Colors.BACKGROUND_COLORS[4] + "  " + Colors.RESET);
+                }
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
