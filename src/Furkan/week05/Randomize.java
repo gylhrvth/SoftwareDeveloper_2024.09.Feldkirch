0c8f5d3a2e9d5022697ad24b22a45684ab3c9f15
Furkan <furkan.bagriacik4646@gmail.com>
2024-10-10 14:24:15 +0200

    Random

---
+++ src/Furkan/week05/Randomize.java
@@ -0,0 +1,21 @@
+package Furkan.week05;
+
+public class Randomize {
+//    public static void main(String[] args) {
+//        String inputText = "Hallo Furkan46";
+//       // String randomizeText=
+//    }
+//
+//    public static String randomText(String text) {
+//        char[] charArray = text.toCharArray();
+//        // char[] randomArray = new char[];
+//
+//
+//
+//
+//    }
+//
+//
+//
+//
+//}
