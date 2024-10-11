fd8fceb860e20e24825e51d8eb650282f88c5ae8
Erik <silverbulllp@gmail.com>
2024-10-11 08:37:13 +0200

    Reverse Aufgabe wie sie sein sollte

--- src/Erik/week05/StringReverse2.java
+++ src/Erik/week05/StringReverse2.java
@@ -6,9 +6,6 @@ package Erik.week05;
                     3. mit text.toCharArray aufsplitten
                     4. new String(charArray) zum zusammenfügen
                     5. Dann aufgesplitteter und wieder zusammengefügter String ausgeben
-                    3. Irgendwie umdrehen mit (replaceAll?)
-                    4. umgedrehter user input mit text.toCharArray ausgeben
-
  */
 
 import java.util.Scanner;
@@ -26,19 +23,18 @@ public class StringReverse2 {
 
     }
 
-    public static void ReverseUserInput(String userInput ){
+    public static void ReverseUserInput(String userInput) {
         char[] charArr = userInput.toCharArray();
 
         for (int i = 0; i < charArr.length / 2; i++) {
             char temp = charArr[i];
             charArr[i] = charArr[charArr.length - 1 - i];
-            charArr[charArr.length - 1 - i]=temp;
+            charArr[charArr.length - 1 - i] = temp;
         }
 
         String reversedUserInput = new String(charArr);
 
         System.out.println(reversedUserInput);
-
     }
 
 }
