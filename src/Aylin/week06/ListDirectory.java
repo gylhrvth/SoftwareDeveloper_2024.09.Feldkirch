c3d67b89cc34d1941c0b7e325ed2444b6adac56b
Aylin Topcu <topcuaylin98@gmail.com>
2024-10-18 09:11:48 +0200

    deleted unnecessary code

--- src/Aylin/week06/ListDirectory.java
+++ src/Aylin/week06/ListDirectory.java
@@ -1,12 +1,10 @@
 package Aylin.week06;
 
 import java.io.File;
-import java.sql.SQLOutput;
-import java.util.Arrays;
 
 public class ListDirectory {
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Aslan Topcu\\IdeaProjects\\SoftwareDeveloper_2024.09.Feldkirch\\src\\Aylin\\week05");
+        File file = new File("C:\\Users\\Aslan Topcu\\IdeaProjects\\SoftwareDeveloper_2024.09.Feldkirch\\src\\Aylin");
         File[] files = file.listFiles();
 
         System.out.println("--------------Sorted List--------------");
@@ -14,7 +12,10 @@ public class ListDirectory {
         printList(files);
         System.out.println("---------List Sorted by length--------");
         sortListLength(files);
+        System.out.println("---------Count and Sum of Files--------");
         printListLength(files);
+
+        countAndSumFiles(files);
 
 //        System.out.println("Name: " + file.getName());
 //        System.out.println("Größe: " + file.length());
@@ -27,15 +28,14 @@ public class ListDirectory {
 //        System.out.println("get path: " + file.getPath());
 //        System.out.println("get absolute path: " + file.getAbsolutePath());
 //        System.out.println("get absolute file: " + file.getAbsoluteFile());
-
     }
 
     public static void sortList(File[] content){
         for (int i = 0; i < content.length ; i++) {
             for (int j = 0; j < content.length -i -1; j++) {
                 if(content[j].isFile() && content[j + 1].isDirectory() ||
-                content[j].isDirectory() && content[j + 1].isDirectory() && content[j].compareTo(content[j + 1]) < 0 ||
-                content[j].isFile() && content[j + 1].isFile() && content[j].compareTo(content[j + 1]) < 0){
+                content[j].isDirectory() && content[j + 1].isDirectory() && content[j].compareTo(content[j + 1]) > 0 ||
+                content[j].isFile() && content[j + 1].isFile() && content[j].compareTo(content[j + 1]) > 0){
                     File temp = content[j];
                     content[j] = content[j + 1];
                     content[j + 1] = temp;
@@ -76,5 +76,19 @@ public class ListDirectory {
                 System.out.printf("%-6s %-35s Size: [%4d]\n", "File:", content[i].getName(), content[i].length());
             }
         }
+    }
+
+    public static void countAndSumFiles(File[] content){
+        int count = 1;
+        int sum = 0;
+        for (int i = 0; i < content.length; i++) {
+            if(content[i].isFile() || content[i].isDirectory()){
+                System.out.printf("Nr. %d: %-35s Size: [%4d]\n", count, content[i].getName(), content[i].length());
+                count++;
+                sum += content[i].length();
+            }
+        }
+        System.out.println("Files: " + (count - 1));
+        System.out.println("Sum length of Files: " + sum);
     }
 }
