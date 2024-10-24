d38cd3b6aac3c752e3064ac233d6ff9450cd8f2f
Furkan <furkan.bagriacik4646@gmail.com>
2024-10-24 10:32:45 +0200

    objektorentierteÜbung

--- src/Furkan/week07/Person/main.java
+++ src/Furkan/week07/Person/main.java
@@ -2,7 +2,7 @@ package Furkan.week07.Person;
 
 public class main {
     public static void main(String[] args) {
-        Human1 human1= new Human1("Shanks",39,199,79);
+        Human1 human1= new Human1("Shanks",39,199,79, 4_000_000_000L);
         human1.printOut();
 
     }
