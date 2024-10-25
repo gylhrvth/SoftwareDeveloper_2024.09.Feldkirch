56d3f41c97738104c45fcb5a18d485fbeef11dd1
Erik <silverbulllp@gmail.com>
2024-10-25 08:47:08 +0200

    data programm start nicht fertig

---
+++ src/Erik/week07/data/DataMain.java
@@ -0,0 +1,20 @@
+package Erik.week07.data;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class DataMain {
+    public static Random rand = new Random();
+
+    public static void main(String[] args) {
+
+        int[] simpleArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        int[] randomArray = new int[];
+
+
+
+
+
+
+    }
+}
