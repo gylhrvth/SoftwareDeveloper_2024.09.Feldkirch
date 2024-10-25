f62f0df8f81d593a7d560b774dde052761f81e90
Furkan <furkan.bagriacik4646@gmail.com>
2024-10-25 12:26:40 +0200

    objektorentierteÜbung Person

--- src/Furkan/week07/Person/Human1.java
+++ src/Furkan/week07/Person/Human1.java
@@ -3,30 +3,49 @@ package Furkan.week07.Person;
 import java.text.NumberFormat;
 
 public class Human1 {
-    private String name;
+    private String firstName;
+    private String surName;
+
+    private String fullName;
     private int age;
     private int size;
     private int weight;
     private long bounty;
    // private String
 
-    public Human1(String name, int age, int height, int weight, long bounty) {
-        this.name = name;
+
+    public Human1(String firstName, String surName, String fullName,
+                  int age, int weight,int size,long bounty ) {
+        this.firstName = firstName;
+        this.surName = surName;
+
+        this.fullName = firstName+ " " + surName;
         this.age = age;
-        this.size = height;
         this.weight = weight;
         this.bounty = bounty;
+        this.size = size;
+    }
+
+    public void createFullName(){
+        this.fullName = firstName + " "+surName;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+
+    public String getName() {
+        return fullName;
     }
 
     public void printOut() {
         NumberFormat formatter = NumberFormat.getInstance();
         String formattedBounty = formatter.format(bounty);
-        System.out.println("Name: " + name + ", " + age + "Jahre alt, " + size + "cm groß und wiegt " + weight + "kg." +
+        System.out.println("Fullname: " + firstName + " " + surName + age + "Jahre alt, " + size + "cm groß und wiegt " + weight + "kg." +
                            " Sein Kopfgeld beträgt " + formattedBounty + " Berry.");
     }
 
     public String toString(){
-        return "Name: " + name + ", " + age + "Jahre alt, " + size + "cm groß und wiegt " + weight + "kg." +
+        return "Fullname: " + firstName + " " +surName + age + "Jahre alt, " + size + "cm groß und wiegt " + weight + "kg." +
                " Sein Kopfgeld beträgt " + bounty + " Berry.";
     }
 }
