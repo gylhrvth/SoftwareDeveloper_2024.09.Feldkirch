ff1f65ce87dc837b7fbd5e6d996a84865e658d79
Akif <akifuenalan@gmail.com>
2024-11-04 09:43:33 +0100

    made optimizations

---
+++ src/Akif_Malik/week07/DataMinMaxAvg/DataMixMax.java
@@ -0,0 +1,11 @@
+package Akif_Malik.week07.DataMinMaxAvg;
+
+public class DataMixMax {
+
+    public int[] Min;
+
+    public int[] Max;
+
+    public int[] Avg;
+
+}
---
+++ src/Akif_Malik/week07/DataMinMaxAvg/DataMixMaxMAIN.java
@@ -0,0 +1,12 @@
+package Akif_Malik.week07.DataMinMaxAvg;
+
+import Akif_Malik.week04.RandomNumberMinMaxAvg;
+
+public class DataMixMaxMAIN {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
