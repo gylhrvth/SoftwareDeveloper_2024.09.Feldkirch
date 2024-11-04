4f0d39f850567cb5145752bdd04b2b470022ff6e
Erik <silverbulllp@gmail.com>
2024-11-04 09:06:35 +0100

    OOP Data nicht fertig

--- src/Erik/week07/data/Data.java
+++ src/Erik/week07/data/Data.java
@@ -1,12 +1,13 @@
 package Erik.week07.data;
 
+import java.util.Random;
+
 public class Data {
 
-    // Attribute
+    // Attribute min max average
     private int minValue;
     private int maxValue;
     private int averageValue;
-
 
     // Konstruktor
     public Data() {
@@ -17,19 +18,83 @@ public class Data {
 
     @Override
     public String toString() {
-        return " ";
+        return "Min Value of the Array is: >> " + minValue +
+                "\nMax Value of the Array is: >> " + maxValue +
+                "\nAverage Value of the Array is: >> " + averageValue;
     }
 
-    public static int[] giveMinValueFromArray(int[] minArray) {
-        int minValue = minArray[0];
+
+    // Getter, Setter
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public int getAverageValue() {
+        return averageValue;
+    }
+
+
+    //eine funktion min, max, avg ausrechnen
+    public static Data calculateEverything(int[] arr) {
+
+        Data test = new Data();
+
+        if (arr[0] > 0) {
+
+            test.minValue = arr[0];
+            test.maxValue = arr[0];
+            test.averageValue = 0;
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.length ; i++) {
+                sum += arr[i];
+                test.minValue = Math.min(test.minValue, arr[i]);
+                test.maxValue = Math.max(test.maxValue, arr[i]);
+            }
+            test.averageValue = sum / arr.length;
+        }
+        return test;
+    }
+
+/*
+    public static int giveMinValueFromArray(int[] minArray) {
+        int minValue = Integer.MAX_VALUE;
 
         for (int i = 0; i < minArray.length; i++) {
             if (minArray[i] < minValue) {
                 minValue = minArray[i];
             }
         }
-        return minArray;
+        return minValue;
     }
 
+    public static int giveMaxValueFromArray(int[] maxArray) {
+        int maxValue = Integer.MIN_VALUE;
 
+        for (int i = 0; i < maxArray.length; i++) {
+            if (maxArray[i] > maxValue) {
+                maxValue = maxArray[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static int giveAverageValueFromArray(int[] avgArray) {
+        int avgValue = 0;
+        int sum = 0;
+
+        for (int value : avgArray) {
+            sum = sum + value;
+            value = value / avgArray.length;
+            avgValue = value;
+        }
+        return avgValue;
+    }
+*/
 }
--- src/Erik/week07/data/DataMain.java
+++ src/Erik/week07/data/DataMain.java
@@ -8,13 +8,49 @@ public class DataMain {
 
     public static void main(String[] args) {
 
-        int[] simpleArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-//        int[] randomArray = new int[];
+        // Array erstellen und befüllen
+        int[] testArr = new int[]{3, 6, 9, 12, 15, 18, 21};
+        int[] randArr = fillArrayWithRandomNumbers(12);
+        int[] crazyArr = fillArrayWithCrazyRandNumbers(12);
 
 
+        // Objekte erstellen
+        Data randResult = Data.calculateEverything(randArr);
+        System.out.println(Arrays.toString(randArr));
+        System.out.println("Random Array Min/Max/Avg\n" + randResult);
+        System.out.println("x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x");
+        Data crazyResult = Data.calculateEverything(crazyArr);
+        System.out.println(Arrays.toString(crazyArr));
+        System.out.println(crazyResult);
 
+
+        // Methoden aufruf
 
 
 
     }
+
+    // Arrays befüllen und zurückgeben
+    public static int[] fillArrayWithRandomNumbers(int arraySize) {
+        int[] randArr = new int[arraySize];
+        for (int i = 0; i < randArr.length; i++) {
+            randArr[i] = rand.nextInt(100);
+        }
+        return randArr;
+    }
+
+    public static int[] fillArrayWithCrazyRandNumbers(int arraySize) {
+        int[] randArr = new int[arraySize];
+        for (int i = 0; i < randArr.length; i++) {
+            randArr[i] = rand.nextInt(-50, 50);
+        }
+        return randArr;
+    }
+
 }
+
+/*
+        Data minValue = new Data(Data.giveMinValueFromArray(testArr));
+        Data maxValue = new Data(Data.giveMaxValueFromArray(testArr));
+        Data avgValue = new Data(Data.giveAverageValueFromArray(testArr));
+ */
