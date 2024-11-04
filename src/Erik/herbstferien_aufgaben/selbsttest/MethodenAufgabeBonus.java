d303fe00d13861dc2f29a8daacf07d6c6e736bc0
Erik <silverbulllp@gmail.com>
2024-11-04 09:26:34 +0100

    Selbsttest Aufgaben

--- src/Erik/herbstferien_aufgaben/selbsttest/MethodenAufgabeBonus.java
+++ src/Erik/herbstferien_aufgaben/selbsttest/MethodenAufgabeBonus.java
@@ -15,11 +15,19 @@ public class MethodenAufgabeBonus {
     public static void printRhombus(String symbol, int size) {
         for (int row = 0; row < size * 2 - 1; row++) {
             for (int col = 0; col < size * 2 - 1; col++) {
-                System.out.print("- ");
+                if (row + col == size - 1) {
+                    System.out.print(symbol);
+                } else if (row - col == size - 1) {
+                    System.out.print(symbol);
+                } else if (row + col == size * 3 - 3) {
+                    System.out.print(symbol);
+                } else if (col - row == size - 1) {
+                    System.out.print(symbol);
+                } else {
+                    System.out.print("-");
+                }
             }
             System.out.println();
         }
-
     }
-
 }
