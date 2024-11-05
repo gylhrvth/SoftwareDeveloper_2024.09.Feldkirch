56129abfcab2fbd7c6802e9b35440003d7ba9171
Gyula Horvath <gylhrvth@gmail.com>
2024-11-05 10:59:42 +0100

    Generator for People

---
+++ src/Gyula/week08/Human/Human2.java
@@ -0,0 +1,33 @@
+package Gyula.week08.Human;
+
+public class Human2 {
+
+    private String name;
+    private int age;
+    private int height;
+    private int weight;
+
+    public Human2(String name, int age, int height, int weight){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + "(" + age + " - " + height + " cm/" + weight +"kg)";
+    }
+}
