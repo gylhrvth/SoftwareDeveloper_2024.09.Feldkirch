bd5b8e601f54fdc0425269cf4e75c6d9d58b8ed5
Aylin Topcu <topcuaylin98@gmail.com>
2024-11-05 10:27:04 +0100

    human2.0

---
+++ src/Aylin/week08/Human2/Human2Main.java
@@ -0,0 +1,11 @@
+package Aylin.week08.Human2;
+
+public class Human2Main {
+    public static void main(String[] args) {
+        Human2 human1 = new Human2("Alfonso", 43, 189, 104);
+        Human2 human2 = new Human2("Karen", 56, 164, 62);
+
+
+        System.out.println(human1);
+    }
+}
