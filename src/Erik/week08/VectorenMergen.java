58bcf3d693d9e320433b0f9b06f234cf27929756
Erik <silverbulllp@gmail.com>
2024-11-05 09:53:18 +0100

    edit

--- src/Erik/week08/VectorenMergen.java
+++ src/Erik/week08/VectorenMergen.java
@@ -28,10 +28,66 @@ public class VectorenMergen {
 //        Vector<Integer> mergedVec = mergeTwoVectors(vec1, vec2);
 //        System.out.println(">>Merged Vector<<\n" + mergedVec);
 
-        Vector<Integer> test2 = test2(vec1, vec2);
+//        Vector<Integer> test2 = test2(vec1, vec2);
+//        System.out.println(">>Merged Vector<<\n" + test2);
+
+        Vector<Integer> test2 = mergeBothVectors(vec1, vec2);
         System.out.println(">>Merged Vector<<\n" + test2);
     }
 
+    public static Vector<Integer> mergeBothVectors(Vector<Integer> vector1, Vector<Integer> vector2) {
+        Vector<Integer> result = new Vector<>();        // Neuer Vektor erstellt
+        int i = 0;
+        int j = 0;
+
+        while (i < vector1.size() && j < vector2.size()) {
+            if (vector1.get(i) >= vector2.get(j)) {
+                result.add(vector1.get(i));
+//                vector1.remove(i);
+                i++; // ++ überspringt eine zahl
+                     // -- error
+            } else {
+                result.add(vector2.get(j));
+//                vector2.remove(j);
+                j++;
+            }
+        }
+
+        while (i < vector1.size()) {
+            result.add(vector1.get(i));
+            i++;
+        }
+
+        while (j < vector2.size()) {
+            result.add(vector2.get(j));
+            j++;
+        }
+
+        return result;
+    }
+
+}
+
+
+
+/*
+    1. Vec = linke Seite
+    2. Vec = rechte Seite
+    3. Vec = Ergebnis
+
+    vom 1. Vec die erste Zahl mit der 1. Zahl vom 2. Vec vergleichen
+    die größer Zahl wird in die 3. Vec gespeichert
+    dann wird die übernommene Zahl in der jeweiligen Vec gelöscht
+    dann wird wieder die 1. Zahl von der 1. und 2. Vec verglichen
+
+   Tools: (.remove, .get, .set) ?
+   ----------------------------------------
+   vergleicht die erste Pos von der linken mit der rechten
+   if (rightSide.get(pos) > leftSide.get(pos)) {
+         result.set(pos, rightSide.get(pos));
+         rightSide.remove(pos);
+
+Erster Versuch:
     public static Vector<Integer> mergeTwoVectors(Vector<Integer> leftSide, Vector<Integer> rightSide) {
         Vector<Integer> result = new Vector<>();                  // neuer Vektor erstellt
         result.setSize(leftSide.size() + rightSide.size());       // result Size wird auf 40 Elemente gestellt
@@ -55,6 +111,7 @@ public class VectorenMergen {
         return result;
     }
 
+Zweiter Versuch eig gleich wie erster:
     public static Vector<Integer> test(Vector<Integer> leftSide, Vector<Integer> rightSide) {
         Vector<Integer> result = new Vector<>();                  // neuer Vektor erstellt
         result.setSize(leftSide.size() + rightSide.size());       // result Size wird auf 40 Elemente gestellt
@@ -71,52 +128,5 @@ public class VectorenMergen {
         }
         return result;
     }
-
-    public static Vector<Integer> test2(Vector<Integer> leftSide, Vector<Integer> rightSide) {
-        Vector<Integer> result = new Vector<>();                  // neuer Vektor erstellt
-        result.setSize(leftSide.size() + rightSide.size());       // result Size wird auf 40 Elemente gestellt
-        int i = 0;
-        int j = 0;
-
-        while (i < leftSide.size() && j < rightSide.size()) {
-            if (leftSide.get(i) <= rightSide.get(j)) {
-                result.add(leftSide.get(i));
-                i++;
-            } else {
-                result.add(rightSide.get(j));
-                j++;
-            }
-        }
-
-        // Füge die restlichen Elemente hinzu
-        while (i < leftSide.size()) {
-            result.add(leftSide.get(i));
-            i++;
-        }
-
-        while (j < rightSide.size()) {
-            result.add(rightSide.get(j));
-            j++;
-        }
-
-        return result;
-    }
-
-/*
-    1. Vec = linke Seite
-    2. Vec = rechte Seite
-    3. Vec = Ergebnis
-
-    vom 1. Vec die erste Zahl mit der 1. Zahl vom 2. Vec vergleichen
-    die größer Zahl wird in die 3. Vec gespeichert
-    dann wird die übernommene Zahl in der jeweiligen Vec gelöscht
-    dann wird wieder die 1. Zahl von der 1. und 2. Vec verglichen
-
-   Tools: (.remove, .get, .set) ?
-   ----------------------------------------
-   vergleicht die erste Pos von der linken mit der rechten
-   if (rightSide.get(pos) > leftSide.get(pos)) {
-         result.set(pos, rightSide.get(pos));
-         rightSide.remove(pos);
 */
-}
+
