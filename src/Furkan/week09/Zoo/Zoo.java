451d3b1f6209a2d223f3d3e9938a10cf2b6785fb
Furkan <furkan.bagriacik4646@gmail.com>
2024-11-12 15:27:15 +0100

    Zoo übung

--- src/Furkan/week09/Zoo/Zoo.java
+++ src/Furkan/week09/Zoo/Zoo.java
@@ -1,5 +1,7 @@
 package Furkan.week09.Zoo;
 
+import Sandro.Colors;
+
 import java.util.HashMap;
 import java.util.Vector;
 
@@ -28,47 +30,83 @@ public class Zoo {
 
     public void printZoo() {
         System.out.println("├──" + name + ", gegründet " + year);
-        for (Gehege gehege : gehegeListe){
+        for (Gehege gehege : gehegeListe) {
             gehege.printZoo();
         }
+        System.out.println("-".repeat(80));
     }
 
-    public void startSimu(){
-        System.out.println("Simulation start ... ");
-        for (int i = 0; i < 14; i++) {
-            System.out.println("Tag (" +i+ ") ....");
-            for (Gehege gehege : gehegeListe){
-                gehege.simu();
+    public void startSimu(int days) {
+        System.out.println("Simulation start für " + days + " Tage...");
+        for (int i = 1; i <= days; i++) {
+            System.out.println(Colors.COLORS[3] + "Tag (" + i + ") beginnt:" + Colors.RESET);
+            for (ZooPfleger zooPfleger : pflegerListe) {
+                zooPfleger.workOnGehege();
             }
+            System.out.println();
         }
 
     }
 
-    public void printFoodCost(){
-        HashMap<Food,Double> foodRequiert = new HashMap<>();
+    public void printFoodCost() {
+        HashMap<Food, Double> foodRequiert = new HashMap<>();
 
-        for (Gehege gehege : gehegeListe){
+        for (Gehege gehege : gehegeListe) {
             gehege.collectFoodRequirement(foodRequiert);
         }
         System.out.println();
 
         System.out.println("Food requirement stats");
         double cost = 0;
-        for (Food food : foodRequiert.keySet()){
-            System.out.println(food.getName() + " ==> " + foodRequiert.get(food) + " " + food.getUnit()+ " / $ per Unit: " + food.getUnitprice());
+        for (Food food : foodRequiert.keySet()) {
+            System.out.println(Colors.COLORS[5] + food.getName() + " ==> " + foodRequiert.get(food) + " " + food.getUnit() + " / $ per Unit: " + food.getUnitprice() + Colors.RESET);
             double amount = foodRequiert.get(food);
             double price = food.getUnitprice() * amount;
-            cost+= price;
+            cost += price;
         }
-        System.out.println("Total costs for all Food requiered: " + cost + " $");
-
+        System.out.println(Colors.COLORS[5] + "Total costs for all Food requiered: " + cost + " $" + Colors.RESET);
     }
 
-    public void addPfleger(ZooPfleger zooPfleger){
-        pflegerListe.add(zooPfleger);
+//    public Gehege seatchAndCreateGehege(String name){
+//        Gehege result = null;
+//        for (Gehege gehege : gehegeListe){
+//            if (result == null && gehege.getName().equals(name)){
+//                result = gehege;
+//            }
+//        }
+//        if (result == null){
+//            result = new Gehege(name);
+//            gehegeListe.add(result);
+//        }
+//        return result;
+//    }
 
+
+    public void searchAndCreateZooKeeper(String name, String gehegeName) {
+        ZooPfleger pfleger = new ZooPfleger(name);
+        for (Gehege gehege : gehegeListe) {
+            if (gehege.getName().equals(gehegeName)) {
+                pfleger.addTask(gehege);
+            }
+        }
+        pflegerListe.add(pfleger);
     }
-
 
 
 }
+//        ZooPfleger result = null;
+//        for (ZooPfleger zooPfleger : pflegerListe) {
+//            if (result == null && zooPfleger.getName().equals(name)) {
+//                result = zooPfleger;
+//            }
+//        }
+//        if (result == null) {
+//            result = new ZooPfleger(name);
+//            pflegerListe.add(result);
+//
+//        }
+//
+//
+//        return result;
+//    }
+
