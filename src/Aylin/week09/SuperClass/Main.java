8f5e5e1d57be46e528ffffea61b9f9cca79d6d60
Aylin Topcu <topcuaylin98@gmail.com>
2024-11-14 10:06:12 +0100

    UML beispiel mit Superklasse

---
+++ src/Aylin/week09/SuperClass/Main.java
@@ -0,0 +1,10 @@
+package Aylin.week09.SuperClass;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Event event = new Event();
+            Concert concert = new Concert();
+            Theater theater = new Theater();
+    }
+}
