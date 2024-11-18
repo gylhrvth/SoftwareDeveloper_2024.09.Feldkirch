7ae56840c929ef8a9a3378e05cbde165c8d18308
Erik <silverbulllp@gmail.com>
2024-11-18 10:45:22 +0100

    fixed

--- src/Erik/week10/Geometry_Interface/Square.java
+++ src/Erik/week10/Geometry_Interface/Square.java
@@ -2,22 +2,8 @@ package Erik.week10.Geometry_Interface;
 
 public class Square extends Rectangle{
 
-    // Attribute
-    private double length;
-
-
     // Konstruktor
-    public Square(double length) {
-        super(length, length);
-    }
-
-
-    // Methoden
-    public double getCircumference() {
-        return 4 * length;
-    }
-
-    public double getArea() {
-        return length * length;
+    public Square(double size) {
+        super(size, size);
     }
 }
