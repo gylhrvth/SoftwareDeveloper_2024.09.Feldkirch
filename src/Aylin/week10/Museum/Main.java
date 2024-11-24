58b71059532f2b71e31b8b3ef3121d7870577106
Aylin Topcu <topcuaylin98@gmail.com>
2024-11-24 23:04:58 +0100

    erweitert aber nicht fertig programmiert

--- src/Aylin/week10/Museum/Main.java
+++ src/Aylin/week10/Museum/Main.java
@@ -1,9 +1,16 @@
 package Aylin.week10.Museum;
 
+import Gyula.week08.Human.Human2;
+
+import java.util.Random;
+import java.util.Vector;
+
 public class Main {
+    public static Random random = new Random();
     public static void main(String[] args) {
         Museum museum = new Museum("Louvre", 1793);
 
+        //Defining rooms
         Showroom room1 = new Showroom("Room 1");
         Showroom room2 = new Showroom("Room 2");
         Showroom room3 = new Showroom("Room 3");
@@ -13,8 +20,40 @@ public class Main {
         Showroom room7 = new Showroom ("Room 7");
         Corridor corridor1 = new Corridor("Corridor 1");
         Corridor corridor2 = new Corridor("Corridor 2");
+        Room entrance = new Room("Entrance");
 
+        //Add next Room to each other - kinda like a door
+        entrance.addNextRoom(corridor1);
 
+        corridor1.addNextRoom(entrance);
+        corridor1.addNextRoom(room1);
+        corridor1.addNextRoom(room4);
+        corridor1.addNextRoom(room7);
+
+        room1.addNextRoom(room2);
+        room1.addNextRoom(corridor1);
+
+        room2.addNextRoom(room1);
+        room2.addNextRoom(room3);
+
+        room3.addNextRoom(room2);
+        room3.addNextRoom(corridor2);
+
+        corridor2.addNextRoom(room3);
+        corridor2.addNextRoom(room4);
+
+        room4.addNextRoom(corridor1);
+        room4.addNextRoom(corridor2);
+        room4.addNextRoom(room7);
+
+        room7.addNextRoom(room4);
+        room7.addNextRoom(corridor1);
+        room7.addNextRoom(room5);
+
+        room5.addNextRoom(room6);
+        room5.addNextRoom(room7);
+
+        //Defining Arts
         ArtObject pMonaLisa = new ArtObject("Mona Lisa", "Leonardo da Vinci", 1503, "Painting");
         ArtObject pStarryNight = new ArtObject("Starry Night", "Vincent van Gogh", 1889, "Painting");
         ArtObject pScream = new ArtObject("The Scream", "Edvard Munch", 1893, "Painting");
@@ -24,7 +63,18 @@ public class Main {
         ArtObject pPersistence = new ArtObject("The Persistence of Memory", "Salvador Dali", 1931, "Painting");
         ArtObject pAngel = new ArtObject("Musician Angel", "Rosso Fiorentino", 1521, "Painting");
 
+        //Defining Guardians
+        Human g1 = new Human("Henrik");
+        Human g2 = new Human("Hans");
 
+
+        int visitorCount = random.nextInt(10);
+        Vector<Visitor> visitorList = new Vector<>();
+        for (int i = 1; i < visitorCount; i++) {
+            visitorList.add(new Visitor("Visitor " + i));
+        }
+
+        //Adding Rooms
         museum.addRoom(room1);
         museum.addRoom(room2);
         museum.addRoom(room3);
@@ -35,6 +85,7 @@ public class Main {
         museum.addRoom(corridor1);
         museum.addRoom(corridor2);
 
+        //Adding Arts to the rooms
         room1.addArt(pMonaLisa);
         room3.addArt(pStarryNight);
         room4.addArt(sDavid);
@@ -44,6 +95,14 @@ public class Main {
         room4.addArt(pPersistence);
         room6.addArt(pAngel);
 
+        //Adding visitors
+        for (Visitor visitor : visitorList) {
+            museum.addVisitor(visitor);
+        }
+
+        //printing Structure and Simulation
         museum.printMuseumName();
+        museum.printMuseumStructure();
+        museum.startSimu();
     }
 }
--- src/Aylin/week10/Museum/Room.java
+++ src/Aylin/week10/Museum/Room.java
@@ -1,16 +1,28 @@
 package Aylin.week10.Museum;
 
+import Gyula.WildColors;
+import Sandro.Colors;
+import java.util.Random;
 import java.util.Vector;
 
 public class Room {
+    public static Random random = new Random();
+
     private String name;
     private Vector<Room> nextRoom;
     private Vector<ArtObject> art;
+    private Vector<Visitor> visitors;
 
     public Room(String name) {
         this.name = name;
         this.nextRoom = new Vector<>();
         this.art = new Vector<>();
+        this.visitors = new Vector<>();
+    }
+
+    public void addNextRoom(Room next){
+        nextRoom.add(next);
+        next.nextRoom.add(this);
     }
 
     public void printRoom(){
@@ -26,11 +38,15 @@ public class Room {
         return art;
     }
 
+    public boolean hasArt() {
+        return !art.isEmpty();
+    }
+
     public void printArt() {
         if (this.getClass().equals(Corridor.class)){
-        }else{
-            if (art.isEmpty()) {
 
+        } else {
+            if (art.isEmpty()) {
                 System.out.println("│   │   ├── new Arts - coming soon!");
             } else {
                 for (ArtObject arts : art) {
@@ -38,5 +54,27 @@ public class Room {
                 }
             }
         }
+    }
+
+    public void dailyTasks(Visitor visitor){
+        if(random.nextInt(100) < 1){
+            System.out.println(Colors.COLORS[2] + visitor.getName() + " is bored and wants to go home." + Colors.RESET);
+        } else {
+            if (visitor != null && hasArt()) {
+                visitor.observeRandomArt(this);
+            }
+        }
+    }
+
+    public void printVisitors() {
+        System.out.println("Currently " + visitors.size() + " visitors in " + name);
+    }
+
+    public void addVisitor(Visitor visitor) {
+        visitors.add(visitor);
+    }
+
+    public void removeVisitor(Visitor visitor) {
+        visitors.remove(visitor);
     }
 }
