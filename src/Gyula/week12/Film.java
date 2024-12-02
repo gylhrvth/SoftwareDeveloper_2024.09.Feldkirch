7a3436235e81cf1591ead563de0250ab968594d7
Gyula Horvath <gylhrvth@gmail.com>
2024-12-02 12:50:31 +0100

    Prototype Film-Actor from DB

---
+++ src/Gyula/week12/Film.java
@@ -0,0 +1,19 @@
+package Gyula.week12;
+
+import java.util.Vector;
+
+public class Film {
+    private int id;
+    private String title;
+    private Actor regie;
+    private Vector<Actor> actorList;
+
+    public Film(){
+        // TODO: Initialize a film
+        actorList = new Vector<>();
+    }
+
+    public void addActor(Actor a){
+
+    }
+}
