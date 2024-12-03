250a1dc5ea5393243c3e1fb97a025096487d702c
Furkan <furkan.bagriacik4646@gmail.com>
2024-12-03 09:55:58 +0100

    datenbank objektorentiert

---
+++ src/Furkan/week12/DatenbankOOP/Film.java
@@ -0,0 +1,19 @@
+package Furkan.week12.DatenbankOOP;
+
+import java.util.Vector;
+
+public class Film {
+    private int id;
+    private String title;
+    private Actor regie;
+    private Vector<Actor> actorList;
+
+    public Film(){
+        // TODO: Initialize a film
+        actorList = new Vector<>();
+    }
+
+    public void addActor(Actor a){
+
+    }
+}
---
+++ src/Furkan/week12/DatenbankOOP/Main.java
@@ -0,0 +1,9 @@
+package Furkan.week12.DatenbankOOP;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Connect DB, SELECT FROM TABLE, Initialize the Object in Java
+
+        // TODO: Create a function for printStructure()
+    }
+}
